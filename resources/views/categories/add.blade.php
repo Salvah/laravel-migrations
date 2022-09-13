f91db0d07bbbb0f03da47e807903566c2c49badb
Thyago Salva <thyago.salva@bento.local>
2022-09-13 17:06:29 -0300

    laravel migrations

---
+++ resources/views/categories/add.blade.php
@@ -0,0 +1,5 @@
+<form action="/categories/store" method="post">
+    @csrf
+    <input type="text" name="nome"/>
+    <button>Enviar</button>
+</form>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/categories/add', function(){
+    return view('categories.add');
+});
+
+Route::post('/categories/store', function(Request $request){
+    $data = $request->all();
+    unset($data['_token']);
+    DB::table('categories')->insert($data);
+});
